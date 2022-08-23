21f0ec0a920981d528d50b933fb786c99fd2f8fb
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-23 10:13:51 +0700

    add home page

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('./router');
 
 const app = express();
+
+// vew engine
+app.set('view engine', 'ejs');
+
+// request body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// router
+app.use(router);
+
+
+app.use((req, res) => {
+    res.render('404');
+})
 
 app.listen(3000);
