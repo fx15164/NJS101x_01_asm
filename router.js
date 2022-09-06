ac47a38abe3fc1fe8a80520d32d2269817096035
fx15164 <khoannfx15164@funix.edu.vn>
2022-09-06 08:31:06 +0700

    complete 3 tab

--- router.js
+++ router.js
@@ -11,6 +11,7 @@ router.get('/', (req, res) => {
       .then(submition => {
          res.render('home', {
             title: 'Trang chủ',
+            path: '/',
             staff: req.staff,
             submition,
             tab
@@ -41,16 +42,77 @@ router.post('/ketthuc', (req, res) => {
 })
 
 router.post('/nghiphep', (req, res) => {
-   const date = new Date(req.body.date)
-   const breakTime = parseInt(req.body.breakTime);
+   const breakTime = parseInt(req.body.breaktime);
    const reason = req.body.reason;
-   Submition.find({
+
+   const _date = new Date(req.body.date)
+   const date = new Date(_date.getFullYear(), _date.getMonth(), _date.getDate());
+
+   Submition.findOne({
       date: date,
       staff: req.staff
    })
-   .then(submition => {
-      console.log(submition);
-   })
+      .then(submition => {
+         if (submition) {
+            return submition;
+         }
+         return Submition.create({
+            date: date,
+            breakTime: 0,
+            staff: req.staff
+         })
+      })
+      .then(submition => {
+         const { annualLeave } = req.staff;
+         if (breakTime > 0 && breakTime <= 8 && breakTime <= annualLeave*8) {
+            submition.breakTime = breakTime;
+            submition.reason = reason;
+            req.staff.annualLeave = (annualLeave*8 - breakTime)/8;
+         }
+         return submition.save();
+      })
+      .then(result => {
+         return req.staff.save();
+      })
+      .then(result => {
+         res.redirect('/?tab=3');
+      })
 })
+
+router.get('/thongtin', (req, res) => {
+   res.render('profile', {
+      title: 'Thông tin cá nhân',
+      path: '/thongtin',
+      staff: req.staff
+   });
+});
+
+router.post('/thongtin', (req, res) => {
+   req.staff.imageUrl = req.body.image;
+   req.staff
+      .save()
+      .then(result => {
+         res.redirect('/thongtin');
+      })
+});
+
+router.get('/giolam', (req, res) => {
+   Submition
+      .find({ staff: req.staff })
+      .then(submitsions => {
+         const salaryMap = new Map();
+         submitsions.forEach(s => {
+            const key = `${s.date.getMonth()}/${s.date.getFullYear()}`;
+            if (!salaryMap[key]) {
+               salaryMap
+            }
+         })
+         res.render('submitsions', {
+            title: 'Giờ làm',
+            path: '/giolam',
+            submitsions: submitsions
+         });
+      })
+});
 
 module.exports = router;
